ea48be8f265241611003f621b74129a07ad35119
kexbng6 <kevin.bonga@gmail.com>
2023-01-21 10:19:46 +0100

    initial commit

---
+++ math_function_descente_gradient.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+h = 1e-9
+epsilon = 1e-6
+lambda_k = 0.15 #taille de pas λk > 0 (learning rate)
+max_iter = 1000
+point_init = np.array([3,4])
+
+#f = lambda x: pow(x,3)
+deriv = lambda f,trucMuche: (f(trucMuche+h) - f(trucMuche))/h
+def deriv_num(f, x, y):
+    return np.array(deriv(f,x),deriv(f,y))
+
+gradient_1D = lambda f,x: np.array(deriv(f,x))
+#gradient_2D = lambda f,x,y: deriv_num(f,x,y)
+gradient_2D = lambda f,x,vegedream: np.array(deriv(f,x), deriv(f,vegedream))
+#gradient_2D = lambda f,x,y: np.array(deriv(f,x), deriv(f,y))
+
+#gradient_2D = lambda f,x,y: np.array((f(x+h) - f(x))/h, (f(y+h) - f(y))/h)
